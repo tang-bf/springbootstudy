bf56d780b9d5adce2a84a20a6e0a4b2df87223a8
tang-bf <1535710733@qq.com>
2020-08-17 23:36:32 +0800

    classloadertst

--- spring.boot.read/src/main/java/com/tbh/demo/devtools/ClassLoaderTest.java
+++ spring.boot.read/src/main/java/com/tbh/demo/devtools/ClassLoaderTest.java
@@ -29,5 +29,48 @@ public class ClassLoaderTest {
 		System.out.println(String.class.getClassLoader());//null
 		//java 核心类库都是引导类加载器加载的
 
+
 	}
+	/**
+	 * protected Class<?> loadClass(String name, boolean resolve)
+	 *         throws ClassNotFoundException
+	 *     {
+	 *         synchronized (getClassLoadingLock(name)) {
+	 *             // First, check if the class has already been loaded
+	 *             从缓存里面找到是否已经加载
+	 *             Class<?> c = findLoadedClass(name);
+	 *             if (c == null) {
+	 *                 long t0 = System.nanoTime();
+	 *                 try {
+	 *                     if (parent != null) {
+	 *                     父加载器加载  递归调用
+	 *                         c = parent.loadClass(name, false);
+	 *                     } else {
+	 *                         c = findBootstrapClassOrNull(name);
+	 *                     }
+	 *                 } catch (ClassNotFoundException e) {
+	 *                     // ClassNotFoundException thrown if class not found
+	 *                     // from the non-null parent class loader
+	 *                 }
+	 *
+	 *                 if (c == null) {
+	 *                     // If still not found, then invoke findClass in order
+	 *                     // to find the class.
+	 *                     long t1 = System.nanoTime();
+	 *                     自定义类加载器加载
+	 *                     c = findClass(name);
+	 *
+	 *                     // this is the defining class loader; record the stats
+	 *                     sun.misc.PerfCounter.getParentDelegationTime().addTime(t1 - t0);
+	 *                     sun.misc.PerfCounter.getFindClassTime().addElapsedTimeFrom(t1);
+	 *                     sun.misc.PerfCounter.getFindClasses().increment();
+	 *                 }
+	 *             }
+	 *             if (resolve) {
+	 *                 resolveClass(c);
+	 *             }
+	 *             return c;
+	 *         }
+	 *     }
+	 */
 }
